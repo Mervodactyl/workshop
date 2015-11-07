78d9eac1a7ddee98e0b1636d2c96303d6856270d
Merve Silk <merve.silk@gmail.com>
2015-11-07 15:16:30 +0000

    creates server on localhost:8888

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-/* *
- * Start by requiring in the http module,
- * and make your own server!
- */
+var http = require("http");
+var fs = require("fs");
 
-"use strict";
+var port = process.env.PORT || 8888;
+
+function handler(request, response) {
+  var url = request.url;
+  if (url.length === 1) {
+    response.writeHead(200, {"Content-Type": "text:html"});
+    response.end("whaaaaaat uuuuuuuuup!!!!!");
+  }
+}
+
+http.createServer(handler).listen(port);
+
+console.log("node http server listening on http://localhost:" + port);
